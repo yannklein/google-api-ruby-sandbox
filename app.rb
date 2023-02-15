49f985d149fa65c8dc8cb0d1579ed7dbb535af80
Yann Klein <yann.klein@me.com>
2023-02-15 17:22:16 +0900

    commented out the model/db part of the app

--- app.rb
+++ app.rb
@@ -6,8 +6,8 @@ require 'dotenv/load'
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'sinatra/activerecord'
-require_relative 'config/application'
+# require 'sinatra/activerecord'
+# require_relative 'config/application'
 
 
 get '/' do
@@ -40,8 +40,8 @@ end
 
 # DO NOT CHANGE BELOW LINES
 # Some configuration for Sinatra to be hosted and operational on Heroku
-after do
-  # Close the connection after the request is done so that we don't
-  # deplete the ActiveRecord connection pool.
-  ActiveRecord::Base.connection.close
-end
+# after do
+#   # Close the connection after the request is done so that we don't
+#   # deplete the ActiveRecord connection pool.
+#   ActiveRecord::Base.connection.close
+# end
